2149f40cbc8f1a27929feb4fbd9a996b7036e928
Bilal Zak <bzak954@gmail.com>
2021-08-08 16:55:51 +0000

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import MovieReview from './Components/MovieReview';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign:'center',fontSize:'60px',marginBottom:'0px'}}>
+        Movie Review App</h1>
+      <p style={{textAlign:'center',fontSize:'20px',color:'grey',marginBottom:'50px'}}>
+        - Using The NewYork Times API -</p>
+         <MovieReview/>
     </div>
   );
 }
